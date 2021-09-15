7716ade4ad3ff3350240b12ed50bd740315c04e2
bdiaz28 <brittanymdiaz@yahoo.com>
2021-09-15 14:30:15 -0400

    project_files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Project-1v3
+
+Kendall Kee, Pat Mills, Brittany Diaz, Mark Marsala
+
+It Is Our WebSites Mission…
+→ To serve as a local resource for the craft beer community in Charlotte, NC. The goal is to provide a simple HTML website that helps residents and visitors explore the Charlotte, NC craft beer/wine/cocktail scene based on events, reviews, & even local weather to effectively aid users in planning their next trip to their local brews and events in the area. 
+User Functionality/Features...
+→ Leave Reviews
+→ Post Pictures
+→ API for Current Local Events
+→ API for Local Weather Updates
+→ API to Generate Food/Beverage Images from Charlotte
+
+//================================================================
+AS A...
+Person in Charlotte, NC, Interested in exploring the craft brewery scene.
+
+I WANT...
+A simple and functional website to check weather, search local breweries, their reviews, local brewery events, and community brewery functions that is updated by users. 
+
+SO THAT...
+I can be better prepared and knowledgeable to explore the craft brewery scene in Charlotte. 
---
+++ assets/script/script.js
@@ -0,0 +1,52 @@
+var breweryContainerEl = document.querySelector("brewery-container");
+
+var api='c8a844d6b509f8578e3b82809031ec29';
+
+var weather=currentWeather();
+function currentWeather() {
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Charlotte,nc,us&units=imperial&appid='+ api)
+.then(function(response) {return response.json()})
+.then(function(data) {
+	console.log(data);
+	var feelsLike = document.getElementById("feelsLike").innerHTML = `Feels Like:${data.main.feels_like}`
+	var humidity = document.getElementById("humidity").innerHTML = `Humidity: ${data.main.humidity}`
+	var temp = document.getElementById("temp").innerHTML = `Temperature: ${data.main.temp}`
+	var tempMax = document.getElementsById ("temp_max").innerHTML = `Temperature Max: ${data.main.temp_max}`
+	var tempMin = document.getElementsById ("temp_min").innerHTML = `Temperature Min: ${data.main.temp_min}`
+	var pressure = document.getElementsById("pressure").innerHTML = `Pressure: ${data.main.pressure}`
+
+
+
+})
+}
+
+
+function breweries(){
+    fetch('https://api.openbrewerydb.org/breweries?by_city=charlotte&sort=name:desc')
+        .then(function(response) {return response.json()})
+        .then(function(data) {
+            breweryList(data);
+            console.log(data);
+
+        })
+    }
+
+var localBreweries = breweries(); 
+
+function breweryList(data){
+var breweryContainerEl = document.getElementById("brewery-container")
+for (var i = 1; i < data.length; i++){
+    var ul = document.createElement("ul");
+ul.innerHTML = data[i].name;
+breweryContainerEl.appendChild(ul);
+};
+}
+		function localEvents(){
+			fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&postalCode=28202&locale=*')
+			.then(function(response) {return response.json()})
+			.then(function(events) {
+				eventList(events);
+				console.log(events);
+    
+			})
+		}
