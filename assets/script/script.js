0719524cbc1a9063a4f25d63bc37367c13fb7b6f
bdiaz28 <brittanymdiaz@yahoo.com>
2021-09-17 10:09:28 -0400

    last refractor

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,7 +8,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Charlotte,nc,us&units=i
 .then(function(response) {return response.json()})
 .then(function(data) {
 	console.log(data);
-	var feelsLike = document.getElementById("feelsLike").innerHTML = `Feels Like:${data.main.feels_like}`
+	var feelsLike = document.getElementById("feelsLike").innerHTML = `Feels Like: ${data.main.feels_like}`
 	var humidity = document.getElementById("humidity").innerHTML = `Humidity: ${data.main.humidity}`
 	var temp = document.getElementById("temp").innerHTML = `Temperature: ${data.main.temp}`
 	var tempMax = document.getElementsById ("temp_max").innerHTML = `Temperature Max: ${data.main.temp_max}`
@@ -22,7 +22,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Charlotte,nc,us&units=i
 
 
 function breweries(){
-    fetch('https://api.openbrewerydb.org/breweries?by_city=charlotte&sort=name:desc')
+    fetch('https://api.openbrewerydb.org/breweries?by_city=charlotte&sort=name:asc')
         .then(function(response) {return response.json()})
         .then(function(data) {
             breweryList(data);
@@ -41,12 +41,3 @@ ul.innerHTML = data[i].name;
 breweryContainerEl.appendChild(ul);
 };
 }
-		function localEvents(){
-			fetch('https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&postalCode=28202&locale=*')
-			.then(function(response) {return response.json()})
-			.then(function(events) {
-				eventList(events);
-				console.log(events);
-    
-			})
-		}
